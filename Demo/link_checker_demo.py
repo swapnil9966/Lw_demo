7fa84835c53708fde6070520ed30c2a237ddbc8e
Swapnil <swapnil.barde97@gmail.com>
2018-04-23 03:14:46 -0700

    link checking with heading

--- Demo/link_checker_demo.py
+++ Demo/link_checker_demo.py
@@ -7,6 +7,7 @@ driver.get("http://istqbexamcertification.com/software-testing-interview-questio
 link = driver.find_element_by_xpath("//*[@title='Verification in software testing']/parent::td//a")
 driver.execute_script("arguments[0].scrollIntoView();",link)
 link = driver.find_element_by_xpath("//*[@title='Verification in software testing']/parent::td//a").get_attribute("href")
+print "Chapter 2. Testing throughout the testing lifecycle contains link -"
 print link
 status_code = requests.get(link).status_code
 if status_code == 200 :
