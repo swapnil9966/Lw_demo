003c16cbffcd9aed460fcfa5a0ccc471c6c12667
Swapnil <swapnil.barde97@gmail.com>
2018-05-09 00:18:15 -0700

    cleartrip demo with pytest

--- pytest_demo/test_cleartrip.py
+++ pytest_demo/test_cleartrip.py
@@ -9,7 +9,8 @@ driver.maximize_window()
 driver.implicitly_wait(30)
 driver.get("https://www.cleartrip.com/")
 time.sleep(9)
-
+#cleartri
+#test
 #oneway = driver.find_element_by_xpath("//input[@value='OneWay']").click()
 origin = driver.find_element_by_xpath("//input[@name='origin']").send_keys("BOM")
 destination = driver.find_element_by_xpath("//input[@name='destination']").send_keys("DEL")
@@ -22,13 +23,24 @@ select = Select(child)
 select.select_by_value('3')
 driver.find_element_by_xpath("//*[@value='Search flights']").click()
 time.sleep(15)
+"""
 driver.find_element_by_xpath("//label[@for='1_1_0']").click()
 driver.find_element_by_xpath("//*[@for='1_1_1']").click()
 driver.find_element_by_xpath("//*[@for='1_1_0_8_departureTime']").click()
 driver.find_element_by_xpath("//*[@original-title='Air India']").click()
+time.sleep(15)
+"""
 buttons = driver.find_elements_by_xpath("//*[@class='resultUnit flightDetailsLink ']//*[@class='booking']")
-book_button = random.sample(buttons,1)
-book_button.click()
+print "Length is "
+print len(buttons)
+try:
+    book_button = random.sample(buttons, 1)
+
+    book_button[0].click()
+except:
+    buttons[0].click()
+
+#driver.find_element_by_xpath("(//*[@class='resultUnit flightDetailsLink ']//*[@class='booking'])[1]").click()
 driver.find_element_by_xpath("//*[@for='insurance_confirm']").click()
 coupon = driver.find_element_by_xpath("//input[@name='coupon']")
 coupon.clear()
