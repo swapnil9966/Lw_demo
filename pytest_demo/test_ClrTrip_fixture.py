64c8822e42f52d6299bb40074e4ea0ddcdf0ed5d
Swapnil <swapnil.barde97@gmail.com>
2018-05-09 03:41:28 -0700

    cleartrip demo with fiture

---
+++ pytest_demo/test_ClrTrip_fixture.py
@@ -0,0 +1,51 @@
+import random
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+
+
+@pytest.fixture(scope="session")
+def launch_browser(request):
+    global driver
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(30)
+    driver.maximize_window()
+    driver.get("https://www.cleartrip.com/")
+    print "Browser Launched Successfully"
+    request.addfinalizer(close_browser)
+
+def close_browser():
+    print "Browser Closed!!!"
+    driver.close()
+
+@pytest.mark.usefixtures("launch_browser")
+class Test(object):
+    def test_search(self):
+        origin = driver.find_element_by_xpath("//input[@name='origin']").send_keys("BOM")
+        destination = driver.find_element_by_xpath("//input[@name='destination']").send_keys("DEL")
+        origin_date = driver.find_element_by_xpath("//input[@title='Depart date']")
+        origin_date.clear()
+        origin_date.send_keys("Thu, 28 Jun, 2018")
+        origin_date.send_keys(Keys.ENTER)
+        child = driver.find_element_by_xpath("//*[@id='Childrens']")
+        select = Select(child)
+        select.select_by_value('3')
+        driver.find_element_by_xpath("//*[@value='Search flights']").click()
+        time.sleep(15)
+        #validations need to be added here for seleccted date , pax counts
+
+    def test_book_flight(self):
+        buttons = driver.find_elements_by_xpath("//*[@class='resultUnit flightDetailsLink ']//*[@class='booking']")
+        book_button = random.sample(buttons, 1)
+        book_button[0].click()
+
+    def test_itinarary(self):
+        driver.find_element_by_xpath("//*[@for='insurance_confirm']").click()
+        coupon = driver.find_element_by_xpath("//input[@name='coupon']")
+        coupon.clear()
+        coupon.send_keys("test")
+
+
